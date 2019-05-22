3ded2f4292c9f78309e9cdbad36bb7054992e884
Luiz dos Reis <luizdosreis@gmail.com>
2019-05-21 21:58:55 -0300

    starting with react

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'strict'
+
+const React = require('react')
+
+const Title = React.createClass({
+    render: function(){
+        return React.createElement('h1',null, 'Titulo')
+    }
+})
+module.exports = Title
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('webpack')
+
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Title = require('./app')
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
+
+
+
