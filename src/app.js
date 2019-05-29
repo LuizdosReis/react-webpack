b7ffac25156dacd720af866b575d585f6429bccd
Luiz dos Reis <luizdosreis@gmail.com>
2019-05-29 20:37:06 -0300

    config lint and add validator web pack

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@
 import React from 'react'
 
 const App = React.createClass({
-    render: function(){
-        return <div>titulo 5</div>
-    }
+  render: function () {
+    return (
+      <h1>titulo 5 </h1>
+    )
+  }
 })
 
 export default App
